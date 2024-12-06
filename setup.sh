9f09cb76e4c667fab639a62b480cc441e5a4fa7e
sangibeaud <stephane.angibeaud@gmail.com>
2024-12-07 00:50:16 +0100

    Some setup.sh improvement.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+test -e local.cfg || echo "Set your local.cfg first !" && exit -1
+
 source local.cfg
 
 mkdir $DATA_REPOSITORY
 git -C $DATA_REPOSITORY init
-echo $DATA_REPOSITORY >> .gitignore
+echo /$DATA_REPOSITORY/ >> .gitignore
 
 #sync to data repository
 # octa-sync.sh
