77e1a99999cac7e2c713e8b8c9fb65548ffadd10
stephane angibeaud <stephane.angibeaud@gmail.com>
2024-12-06 22:01:33 +0100

    Sync path included

--- octa-sync.sh
+++ octa-sync.sh
@@ -1,7 +1,7 @@
 source local.cfg
 
 #rsync -vnrc --exclude Base/AUDIO /run/media/stephane/OCTATRACK64/ OT/
-RSYNC_CMD="rsync -vrc --exclude Base/AUDIO $OT_MOUNT/$OT_PROJECT_PATH/$OT_PROJECT $DATA_REPOSITORY/"
+RSYNC_CMD="rsync -vrc --exclude Base/AUDIO $OT_MOUNT/$OT_PROJECT_PATH/$OT_PROJECT $DATA_REPOSITORY/$OT_PROJECT_PATH"
 
 echo $RSYNC_CMD
 echo 2 secs to cancel \(ctrl-C\)
@@ -9,5 +9,6 @@ sleep 1
 echo 1 secs to cancel \(ctrl-C\)
 sleep 1
 echo $RSYNC_CMD
+mkdir -p $DATA_REPOSITORY/$OT_PROJECT_PATH
 eval $RSYNC_CMD
 
