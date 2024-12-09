4440ffea959d0246bf150f388525254a58731eef
stephane angibeaud <stephane.angibeaud@gmail.com>
2024-12-09 21:13:11 +0100

    Save log reports.

--- octa-rpt.sh
+++ octa-rpt.sh
@@ -1,3 +1,8 @@
 source local.cfg
 
-git -C $DATA_REPOSITORY/ diff `git -C OT/ log -n 2 | grep commit | sort -r | cut -f 2 -d " "`
+RPT_DATA_PATH=./data/reports
+RPT_FILE=$RPT_DATA_PATH/rpt-`date +%m%d_%H%M.txt`
+
+git -C $DATA_REPOSITORY/ log | head -n 5 | tail -n 1 >> $RPT_FILE
+git -C $DATA_REPOSITORY/ diff `git -C $DATA_REPOSITORY/ log -n 2 | grep commit | sort -r | cut -f 2 -d " "`  >> $RPT_FILE
+
